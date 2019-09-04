0fe465d7bcc03030c845daae74b5c646babd43e7
skennel2 <skennel2@gmail.com>
2019-09-04 15:35:59 +0900

    react-tap-event-plugin

--- src/testClientRoot.js
+++ src/testClientRoot.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import { BrowserRouter as Router, Route, Link, Switch, withRouter } from "react-router-dom";
 import { TestComponentTest } from './example';
+
+// React Tap Plugin
+injectTapEventPlugin();
 
 // 샘플 페이지가 추가되면 아래에 { pageName : '', pageComponent: object } 형태로
 // 추가해주세요.
